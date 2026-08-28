988da57fce9a2f88cd8e26381cecb8ac7cf15eb5
Shahzad <hprshahzad29@gmail.com>
2026-08-28 13:23:15 +0330

    chore: setup admin panel folder structure

---
+++ src/pages/TicketsDetails/TicketsDetails.jsx
@@ -0,0 +1,7 @@
+function TicketsDetails() {
+  return (
+    <div>TicketsDetails</div>
+  )
+}
+
+export default TicketsDetails
---
+++ src/routes.jsx
@@ -0,0 +1,52 @@
+import { createBrowserRouter, Outlet } from "react-router";
+import Home from "./pages/Home/Home"
+import Product from "./pages/Products/Products"
+import Tickets from "./pages/Tickets/Tickets"
+import TicketsDetails from "./pages/TicketsDetails/TicketsDetails"
+import Users from "./pages/Users/Users"
+import Comments from "./pages/Comments/Comments"
+import NotFound from "./pages/NotFound/NotFound"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <>
+                Layout
+                <Outlet/>
+            </>
+        ),
+        children : [
+            {
+                index: true,
+                element: <Home/>
+            },
+            {
+                path: "/Product",
+                element: <Product/>
+            },
+            {
+                path: "/Users",
+                element: <Users/>
+            },
+            {
+                path: "/Tickets",
+                element: <Tickets/>
+            },
+            {
+                path: "/Comments",
+                element: <Comments/>
+            },
+            {
+                path: "/TicketsDetails",
+                element: <TicketsDetails/>
+            },
+            {
+                path: "/*",
+                element: <NotFound/>
+            },
+        ]
+    },
+])
+
+export default router
